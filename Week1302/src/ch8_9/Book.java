319435bf94f1b86b876efe500ca2246f1f27c404
vic <h88001166@gmail.com>
2023-06-09 10:24:51 +0800

    Add book enum.

---
+++ Week1302/src/ch8_9/Book.java
@@ -0,0 +1,18 @@
+package ch8_9;
+
+public enum Book {
+    JHTP("Java How to Program", "2015"),
+    CHTP("C How to Program", "2013"),
+    IW3HTP("Internet & World Wide Web How to Program", "2012"),
+    CPPHTP("C++ How to Program", "2014"),
+    VBHTP("Visual Basic How to Program", "2014"),
+    CSHARPHTP("Visual C# How to Program", "2014");
+
+    public final String title;
+    public final String copyRightYear;
+
+    Book(String title, String copyRightYear) {
+        this.title = title;
+        this.copyRightYear = copyRightYear;
+    }
+}
