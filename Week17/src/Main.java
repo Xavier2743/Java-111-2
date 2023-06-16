f905a5dfceb391f93e823ef69ab99df351a2e1d8
vic <h88001166@gmail.com>
2023-06-16 10:35:00 +0800

    Object reference concept.

--- Week17/src/Main.java
+++ Week17/src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 0;
+        add(a);
+        System.out.println(a);
+
+        MyClass myClass = new MyClass();
+        add(myClass);
+        System.out.println(myClass.a);
+
+        String str = "123";
+        add(str);
+        System.out.println(str);
     }
+
+    public static void add(int a) {
+        a++;
+    }
+
+    public static void add(MyClass myClass) {
+        myClass.a++;
+    }
+
+    public static void add(String str) {
+        str += "456";
+    }
+}
+
+class MyClass {
+    int a;
 }
